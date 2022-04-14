efa7b32fed8bbecb9e4fa8f31acbe061e126828d
Shahzaib Ibrahim <shahzaib.ibrahim@forwardyou.com>
2022-04-14 05:33:50 +0500

    Replacing Redux With Context Api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import ProductsContextProvider from './context/products-context';
 import './index.css';
 import App from './App';
 import productReducer from './store/reducers/products';
@@ -15,10 +15,12 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 ReactDOM.render(
+  <ProductsContextProvider>
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
+  </Provider>
+  </ProductsContextProvider>,
   document.getElementById('root')
 );
